047c7b45b7599b224c80febf30d6f824c84718d8
hd9s1lk <hd9business@gmail.com>
2023-11-08 00:02:39 +0000

    teste

---
+++ Estudo/2/escola3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "escola.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+    Professor prof1("Iracema", 48, "Matematica");
+
+    cout << prof1;
+
+    system("pause");
+    return 0;
+}
